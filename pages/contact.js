3e72683eb61591019ead1ce787ae935537df2785
Kiloechoalpha <letlhake.kv@gmail.com>
2024-02-28 16:10:21 +0200

    Update contacts

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ContactForm from '../components/contacts/contact-form';
+import ContactForm from 'components\contacts\contact-form.js'; 
 
 
 function ContactPage() {
   return (
     <div>
-      <h1>If you want some tips and tricks, contact us</h1>
+      <h1>Please Contact Us</h1>
       <ContactForm />
     </div>
   );
