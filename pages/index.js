c04460e25da29d5cfc45c7c69d0e24a6760e4497
Kealeboga Letlhake <125870519+Kiloechoalpha@users.noreply.github.com>
2024-03-01 09:16:33 +0200

    Update index.js
    Add Documentation

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
 import { Fragment } from "react";
-import FeaturedPosts from "../components/home-page/featured-posts";
-import Hero from "../components/home-page/hero";
-import { getFeaturedPosts } from "../lib/post-util";
+import FeaturedPosts from "../components/home-page/featured-posts"; // Importing FeaturedPosts component
+import Hero from "../components/home-page/hero"; // Importing Hero component
+import { getFeaturedPosts } from "../lib/post-util"; // Importing getFeaturedPosts function
 
-
-
+// Functional component for the Home page
 function HomePage(props) {
     return(
         <Fragment>
+            {/* Rendering the Hero component */}
             <Hero />
+            {/* Rendering the FeaturedPosts component with posts passed as props */}
             <FeaturedPosts posts={props.posts} />
         </Fragment>
     );
 }
+
+// Function to fetch static props for the Home page
 export function getStaticProps(){
+    // Fetching featured posts using getFeaturedPosts function
     const FeaturedPosts = getFeaturedPosts();
     return {
+        // Returning props with featured posts
         props: {
             posts: FeaturedPosts
         }
