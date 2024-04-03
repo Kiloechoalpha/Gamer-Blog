0b3a735732cb1b2a127e723ccec4e2c9fa9dd762
Kealeboga Letlhake <125870519+Kiloechoalpha@users.noreply.github.com>
2024-04-03 16:34:28 +0200

    Update index.js
    Add documentation

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,24 +1,30 @@
 import AllPosts from "../../components/posts/all-posts";
-import { getAllPosts } from "../../lib/post-util"; 
+import { getAllPosts } from "../../lib/post-util";
 
-
-
+/**
+ * Renders a page displaying all posts.
+ * @param {object} props - The props object containing posts to be displayed.
+ * @returns {JSX.Element} - Returns JSX to render the AllPosts component with the provided posts.
+ */
 function AllPostsPage(props) {
   return (
-
-      <AllPosts posts={props.posts}/>
-
-  )
+    <AllPosts posts={props.posts}/>
+  );
 }
-export function getStaticProps(){
 
- const allPosts = getAllPosts();
+/**
+ * Retrieves all posts to be displayed on the page.
+ * @returns {object} - Returns an object containing props with all posts.
+ */
+export function getStaticProps() {
+  // Retrieve all posts
+  const allPosts = getAllPosts();
 
- return {
-  props: {
-    posts: allPosts
-  }
- };
+  return {
+    props: {
+      posts: allPosts
+    }
+  };
 }
 
 export default AllPostsPage;
