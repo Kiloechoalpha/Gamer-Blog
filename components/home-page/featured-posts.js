3be67bd11cd6bcb14a21cbc6e0d9707ea68eaf4c
Kealeboga Letlhake <125870519+Kiloechoalpha@users.noreply.github.com>
2024-04-04 22:11:05 +0300

    Update featured-posts.js

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -4,7 +4,7 @@ import classes from "./featured-posts.module.css";
 function FeaturedPosts(props) {
   return (
     <section className={classes.latest}>
-      <h2>Featured Posts</h2>
+      <h2>Posts</h2>
       <PostGrid posts={props.posts} />
     </section>
   );
